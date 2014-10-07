448f529f41d112fe69c62ea8ed1df099eb5ac18d
Juanber <juanber84@gmail.com>
2014-10-07 23:14:52 +0200

    Rename name of proyect in documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'pushiner'
+project = u'Pushiner'
 copyright = u'2014, fourcoders'
 
 # The version info for the project you're documenting, acts as replacement for
